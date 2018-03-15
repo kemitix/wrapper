ebf9ae2fc975480ea52b2f2745e32c02dd6b39de
Paul Campbell <pcampbell@kemitix.net>
2018-03-15 19:58:22 +0000

    Update license

--- src/main/java/net/kemitix/wrapper/WrapperState.java
+++ src/main/java/net/kemitix/wrapper/WrapperState.java
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2017 Paul Campbell
+ * Copyright (c) 2018 Paul Campbell
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
  * and associated documentation files (the "Software"), to deal in the Software without restriction,
