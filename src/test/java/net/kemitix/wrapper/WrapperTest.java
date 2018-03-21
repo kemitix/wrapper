d8c6cdfcd7f7503b726612d1255e3e6d89e4163f
Paul Campbell <pcampbell@kemitix.net>
2018-03-21 20:58:48 +0000

    Update test

--- src/test/java/net/kemitix/wrapper/WrapperTest.java
+++ src/test/java/net/kemitix/wrapper/WrapperTest.java
@@ -24,7 +24,7 @@ public class WrapperTest {
     public void wrapRequiresInner() {
         assertThatNullPointerException()
                 .isThrownBy(() -> Wrapper.wrap((Wrapper) null))
-                .withMessage("inner");
+                .withMessage("wrapper");
     }
 
     @Test
